2e913e8a68dcefba1ba72228c83444e9e0324ee2
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-04-25 00:16:06 +0500

    pagination 2

--- common_app/permissions.py
+++ common_app/permissions.py
@@ -1,2 +1,46 @@
 from rest_framework.permissions import BasePermission
 
+
+
+# Faqat admin yoki staff foydalanuvchilar kirishi mumkin bo'lgan permission klass
+class AminUser(BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        return request.user.is_authenticated and request.user.is_admin or request.user.is_staff
+    
+
+
+# ========================================================================================
+
+# Faqat staff, o'qituvchi yoki admin foydalanuvchilarga ruxsat beruvchi permission klass.
+# Foydalanuvchi login bo‘lgan va shu 3 roldan biriga ega bo‘lishi kerak.
+
+class AdminOrTeacher(BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        return request.user.is_authenticated and (request.user.is_staff or request.user.is_teacher or request.user.is_admin)
+    
+
+# ===========================================================================================
+
+# Faqat O'quvchi ,admin ,staff ga ruxsat bor
+class AdminOrStudent(BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        return request.user.is_authenticated and (request.user.is_admin or request.user.is_student or request.user.is_staff)
+
+
+# ============================================================================================
+
+# Custom permission klass:
+# - Faqat obyekt egasi (obj.user == request.user)
+# - yoki admin/staff foydalanuvchilar obyektga ruxsat oladi.
+# - Foydalanuvchi login bo‘lgan bo‘lishi shart.
+class AdminOrOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if not request.user.is_authenticated:
+            return False
+        return request.user.is_authenticated and (obj.user == request.user or request.user.is_staff or request.user.is_admin)
