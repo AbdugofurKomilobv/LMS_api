dee57861a509407027f296e09ff0a4f9d201c098
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-04-24 23:54:32 +0500

    permissions

--- common_app/pagination.py
+++ common_app/pagination.py
@@ -1,1 +1,16 @@
 from rest_framework.pagination import PageNumberPagination
+
+
+
+# Umumiy pagination sozlamalari
+class Pagination(PageNumberPagination):
+    page_size = 25  # Default sahifa o'lchami
+    page_size_query_param = "page_size"  # URL orqali o'lchamni o'zgartirish parametri
+    max_page_size = 55  # Maksimal sahifadagi obyektlar soni
+
+
+# Talabalar davomati uchun sahifalash sozlamalari
+class StudentAttendancePagination(PageNumberPagination):
+    page_size = 1  # Default sahifadagi elementlar soni
+    page_query_param = 'page_size'  # URL orqali page_size ni o'zgartirish imkoniyati
+    max_page_size = 55  # Maksimal ruxsat etilgan sahifa o'lchami
