cb1aaf98a9fe0c4ee070a5f1ff9ba28b2d68f36b
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-04-26 23:06:07 +0500

    q

--- user_app/serializers.py
+++ user_app/serializers.py
@@ -4,3 +4,8 @@ from django.contrib.auth.hashers import make_password
 
 from courses_app.models import Group
 from user_app.models import Teacher,User,Student,Parent
+
+class UserAllSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
