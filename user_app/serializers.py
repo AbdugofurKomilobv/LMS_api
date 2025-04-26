622900ecc34344fc2692e42734c315b1d1827fe2
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-04-26 23:06:50 +0500

    sd

--- user_app/serializers.py
+++ user_app/serializers.py
@@ -9,3 +9,8 @@ class UserAllSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id","password","full_name","phone")
