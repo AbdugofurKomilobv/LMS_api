942af12645d13dc5f8758fdcc8990a5b369ab911
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-05-04 14:50:45 +0500

    sss

--- homework_app/admin.py
+++ homework_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Homework, HomeworkAnswer
+
+class HomeworkAnswerInline(admin.TabularInline):
+    model = HomeworkAnswer
+    extra = 1
+
+class HomeworkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'lesson', 'group', 'deadline')
+    search_fields = ('title', 'lesson__title', 'group__title')
+    list_filter = ('lesson', 'group', 'deadline')
+    inlines = [HomeworkAnswerInline]
+
+class HomeworkAnswerAdmin(admin.ModelAdmin):
+    list_display = ('student', 'homework', 'submitted_at', 'is_graded', 'grade')
+    list_filter = ('is_graded', 'submitted_at', 'homework')
+    search_fields = ('student__user__phone', 'homework__title')
+
+admin.site.register(Homework, HomeworkAdmin)
+admin.site.register(HomeworkAnswer, HomeworkAnswerAdmin)
--- payments_app/admin.py
+++ payments_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Payment
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('student', 'amount', 'status', 'payment_method', 'created_at', 'confirmed_by')
+    search_fields = ('student__user__full_name', 'student__user__phone', 'status', 'payment_method')
+    list_filter = ('status', 'payment_method', 'created_at')
+    date_hierarchy = 'created_at'
+    readonly_fields = ('created_at', 'confirmed_by')
+
+admin.site.register(Payment, PaymentAdmin)
